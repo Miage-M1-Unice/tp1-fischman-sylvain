aa1872fa14d43535d64c303760bab361fc532793
sylvain F <sylvain.fischman@etu.unice.fr>
2019-03-26 10:00:56 +0100

    suite tp3 ex 2

--- src/miage/tp3/MyClassLoader.java
+++ src/miage/tp3/MyClassLoader.java
@@ -1,6 +1,8 @@
 package miage.tp3;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.SecureClassLoader;
 import java.util.ArrayList;
 
@@ -12,23 +14,36 @@ public class MyClassLoader extends SecureClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         byte[] b = loadClassData(name);
-        System.out.println("name = [ "+ name + " ]");
+        //System.out.println("name = [ "+ name + " ]");
+
+        if (b == null){
+            throw new ClassNotFoundException();
+        }
+
         return super.defineClass(name, b, 0, b.length);
 
     }
 
     private byte[] loadClassData(String name) throws ClassNotFoundException {
-        // TODO  A COMPLETER
-        // transformer le name en chemin .class
-        //ouvrir le fichier et le lire
-        //retourner le contenu
-        name = name.replace(".","/"); // chercher pathseparator
-        System.out.println(name);
+        for (int i = 0; i < path.size(); i++){
+            //System.out.println(path.get(i));
+            if (path.get(i).exists()){
+                name = path.get(i) + File.separator + name.replace(".",File.separator) + ".class";
+                //System.out.println(name);
+                try {
+                    byte[] fileContent = Files.readAllBytes(Paths.get(name));
+                    return fileContent;
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 
     public static void main(String[] args) {
         ArrayList<File> al = new ArrayList<File>();
+        al.add(new File("/home/sylvain/Documegfffffffffffffffffffffffffffffffffffffgnts/cours/M1/tp/out/production/tp/"));
         al.add(new File("/home/sylvain/Documents/cours/M1/prog-avancee/tp/out/production/tp/"));
         MyClassLoader myCL = new MyClassLoader(al);
         try {
